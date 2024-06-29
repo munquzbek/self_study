6ef2e9f2b99431129fced2a0fa929c6f5f1ec2d3
munquzbek <140368524+munquzbek@users.noreply.github.com>
2024-06-28 22:25:29 -0400

    + model Course, Lesson

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 
     'users',
+    'lms',
 ]
 
 MIDDLEWARE = [
---
+++ lms/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='владелец',
+                              help_text='укажите владельца курса')
+    last_update = models.DateTimeField(auto_now=False, verbose_name='последнее обновление', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    url = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', related_name='course')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='владелец',
+                              help_text='укажите владельца курса')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
